749471ff3443c16fce7e1f9bf57f250c15b3d021
willisxiong <xiongwei.fly@gmail.com>
2022-09-28 01:06:02 +0800

    second commit, add remove buttons and can add gw and cpe selection

--- js/script.js
+++ js/script.js
@@ -1,28 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#quote1').addEventListener('click', () => {
             var request = new XMLHttpRequest;
-            request.open('GET', '/cpe_price.json', true);
+            request.open('GET', 'cpe_price.json', true);
             request.send();  
 
             request.onload = () => {
                 const data = JSON.parse(request.responseText);
-                console.log(data);
-
-                var city = document.getElementById('city-select').value
-                console.log(city);
 
                 var price_content = data['HK']['5m']['VE100b'];
-                document.querySelector('#sale-price').innerHTML += `<span id="price_content">${price_content}</span>`;
+                document.querySelector('#sale-price').insertAdjacentHTML("afterend", `<span id="price_content">${price_content}</span>`);
             };
         });
+    
+    let counter = 0;
 
     document.querySelector("#add-gw").addEventListener("click", () => {
+        counter++;
+
         var city_select = document.querySelector("#gw-city-select").innerHTML;
         var bw_select = document.querySelector("#gw-bw-select").innerHTML;
-        var remove_button = "<button type='button' id='remove'>remove</button>"
-        const add_content = `<div class="row"><div class="solution-content"><select>${city_select}</select></div><div class="solution-content"><select>${bw_select}</select></div>` + remove_button + `</div>`;
+        var remove_button = `<button type='button' class='remove-button' id='${counter}'>remove</button>`
+        const add_content = `<div class="row" id="${counter}"><div class="solution-content"><select>${city_select}</select></div><div class="solution-content"><select>${bw_select}</select></div>` + remove_button + `</div>`;
 
         document.querySelector("#solution1_gw").insertAdjacentHTML("afterend" ,add_content);
+
+        let buttons = document.querySelectorAll(".remove-button");
+        console.log(buttons);
+        buttons.forEach(element => {
+            element.addEventListener("click", () => {
+                console.log(element);
+                const row_element = document.getElementById(element.id).getElementsByClassName(".row");
+                row_element.remove();
+            });
+        });
 
     });
 
@@ -30,9 +40,13 @@ document.addEventListener("DOMContentLoaded", () => {
         var cpe_city_select = document.querySelector("#cpe-city-select").innerHTML;
         var cpe_bw_select = document.querySelector("#cpe-bw-select").innerHTML;
         var cpe_device_select = document.querySelector("#cpe-device-select").innerHTML;
-        var remove_button = "<button type='button' id='remove'>remove</button>";
+        var remove_button = "<button type='button' class='remove-button' id='remove'>remove</button>";
         const add_content = `<div class="row"><div class="solution-content"><select>${cpe_city_select}</select></div><div class="solution-content"><select>${cpe_bw_select}</select></div><div class="solution-content"><select>${cpe_device_select}</select></div>` + remove_button + `</div>`;
 
         document.querySelector("#solution1_cpe").insertAdjacentHTML("afterend", add_content);
     });
+
+    console.log(counter);
+
+    
 });
